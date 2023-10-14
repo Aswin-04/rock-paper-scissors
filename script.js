721b41ce0bcba32892c7f05f7d56458396848317
Aswin-04 <e.aswin004@gmail.com>
2023-10-14 13:15:10 +0530

    Finish project rock-paper-scissor

--- script.js
+++ script.js
@@ -1,92 +1,92 @@
-    // create a getComputerChoice() Which randomly return either "Rock", "Paper" or "Scissor"
-    function getComputerChoice() {
-        let randomNum = Math.floor(Math.random() * 3);
 
-        switch(randomNum) {
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
 
-            case 0:
-                return "rock"
-            
-            case 1:
-                return "paper"
-            
-            default:
-                return "scissor"
-        }
-    }
+    switch(randomNum) {
 
-    
-    // create a playRound() which takes two parameters the playerSelection and the computerSelection
-    // that should return the string that declares the winner of the round
-    function playRound(computerSelection, playerSelection) {
-        if (computerSelection == playerSelection) {
-            return "Oops... Its a tie"
-            
-        } else if(playerSelection == "paper" && computerSelection == "rock") {
-            playerWins+=1;
-            return "You won!! paper beats rock"
-            
-        } else if (playerSelection == "rock" && computerSelection == "scissor") {
-            playerWins+=1;
-            return "You won!! rock beats scissor"
-            
-        } else if (playerSelection == "scissor" && computerSelection == "paper") {
-            playerWins+=1;
-            return "You won!! scissor beats paper"
-            
-        } else if (computerSelection == "scissor" && playerSelection == "paper") {
-            computerWins+=1;
-            return "You lost! scissor beats paper"
-            
-        } else if (computerSelection == "paper" && playerSelection == "rock") {
-            computerWins+=1;
-            return "You lost! paper beats rock"
-            
-        } else if (computerSelection == "rock" && playerSelection == "scissor") {
-            computerWins+=1;
-            return "you lost! rock beats scissor"
-
-        } else {
-            return "Invalid input"
-        }
-    }
-    
-    
-    let playerWins = 0;
-    let computerWins = 0;
-    
-    // create a new function game() that uses the previous function inside of this function to play
-    // a 5 round game
-    function game() {
+        case 0:
+            return "rock"
         
-        let computerSelection = getComputerChoice();        
-        let playerSelection = prompt("Choose one (rock, paper, scissor)").trim().toLowerCase();
-        console.log(playRound(computerSelection, playerSelection));
-
-        computerSelection = getComputerChoice();        
-        playerSelection = prompt("Choose one (rock, paper, scissor)").trim().toLowerCase();
-        console.log(playRound(computerSelection, playerSelection));
-
-        computerSelection = getComputerChoice();        
-        playerSelection = prompt("Choose one (rock, paper, scissor)").trim().toLowerCase();
-        console.log(playRound(computerSelection, playerSelection));
-
-        computerSelection = getComputerChoice();        
-        playerSelection = prompt("Choose one (rock, paper, scissor)").trim().toLowerCase();
-        console.log(playRound(computerSelection, playerSelection));
-
-        computerSelection = getComputerChoice();        
-        playerSelection = prompt("Choose one (rock, paper, scissor)").trim().toLowerCase();
-        console.log(playRound(computerSelection, playerSelection));
-
-
-        if(playerWins == computerWins) {
-            console.log("Its a Draw");
-        } else if(playerWins > computerWins) {
-            console.log("Congrats, you win");
-        } else {
-            console.log("Computer wins");
-        }
+        case 1:
+            return "paper"
+        
+        default:
+            return "scissor"
     }
+}
 
-    game()
+
+function playRound(computerSelection, playerSelection) {
+    
+    if (computerSelection == playerSelection) {
+        result.textContent = "Oops... Its a tie"
+        
+    } else if(playerSelection == "paper" && computerSelection == "rock") {
+        playerWins+=1;
+        result.textContent = "You won!! paper beats rock"
+        
+    } else if (playerSelection == "rock" && computerSelection == "scissor") {
+        playerWins+=1;
+        result.textContent = "You won!! rock beats scissor"
+        
+    } else if (playerSelection == "scissor" && computerSelection == "paper") {
+        playerWins+=1;
+        result.textContent = "You won!! scissor beats paper"
+        
+    } else if (computerSelection == "scissor" && playerSelection == "paper") {
+        computerWins+=1;
+        result.textContent = "You lost! scissor beats paper"
+        
+    } else if (computerSelection == "paper" && playerSelection == "rock") {
+        computerWins+=1;
+        result.textContent = "You lost! paper beats rock"
+        
+    } else if (computerSelection == "rock" && playerSelection == "scissor") {
+        computerWins+=1;
+        result.textContent = "you lost! rock beats scissor"
+    }
+    
+    playerScore.textContent = playerWins;
+    computerScore.textContent = computerWins;
+
+    if (computerWins === 5) {
+        result.textContent = "GAME OVER - You lost! Computer Won";
+        computerWins = 0;
+        playerWins = 0;
+
+    } else if (playerWins === 5) {
+        result.textContent = "GAME OVER - You Won! Computer lost";
+        computerWins = 0;
+        playerWins = 0;
+    }
+}
+
+let playerWins = 0;
+let computerWins = 0;
+
+
+let div = document.body.querySelector(".container-1")
+let rock = div.querySelector(".rock-img");
+let paper = div.querySelector(".paper-img");
+let scissor = div.querySelector(".scissor-img");
+
+let result = document.body.querySelector(".result-info");
+let playerScore = document.body.querySelector(".player-score");
+let computerScore = document.body.querySelector(".computer-score");
+
+rock.addEventListener("click",(e) => {
+    let element = e.target
+    playRound(getComputerChoice(), element.dataset.rock)
+});
+
+paper.addEventListener("click",(e) => {
+    let element = e.target
+    playRound(getComputerChoice(), element.dataset.paper)
+});
+
+scissor.addEventListener("click",(e) => {
+    let element = e.target
+    playRound(getComputerChoice(), element.dataset.scissor)
+});
+
+
